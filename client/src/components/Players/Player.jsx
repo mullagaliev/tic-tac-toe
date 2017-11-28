a28092691c2f9504ceb702982132f64f4f5dbe02
andrey.m <andrey.m@fora-soft.com>
2017-11-28 12:56:19 +0300

    refactoring players component
    1. add player component
    2. edit players component

---
+++ client/src/components/Players/Player.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Players.sass';
+
+
+class Player extends Component {
+  render() {
+    return (<div
+      className={'b-player b-player--' + this.props.position + (this.props.active ? ' current' : '') }
+      key={this.props.id}>
+      <div className="b-player__name">
+        <span>
+          { this.props.current ? 'You (' + this.props.name + ')' : this.props.name }
+          <br/>
+        </span>
+      </div>
+      {/* TODO create score component */}
+      <div className="b-player__score">
+        {this.props.score}
+      </div>
+      <div className="b-player__avatar">
+        <img
+          // TODO move to static folder
+          src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/720/ninja-background-128.png"
+          className="b-player__avatar-image"/>
+        <div className="b-player__marker c-mark" value={ this.props.marker }>
+          <Icon className="c-mark__x" name='remove' size="big" color="pink"/>
+          <Icon className="c-mark__o" name='circle thin' size="big" color="blue"/>
+        </div>
+      </div>
+    </div>);
+  }
+}
+
+Player.propTypes = {
+  position: PropTypes.string,
+  active: PropTypes.bool,
+  current: PropTypes.bool,
+  name: PropTypes.string,
+  score: PropTypes.number,
+  marker: PropTypes.string
+};
+Player.defaultProps = {
+  position: 'client',
+  active: false,
+  current: false,
+  name: 'Guest',
+  score: 0,
+  marker: '_'
+};
+
+export default Player;
--- client/src/components/Players/Players.js
+++ client/src/components/Players/Players.js
@@ -1,84 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Icon, Popup } from 'semantic-ui-react';
 import './Players.sass';
+import Player from './Player';
 
-class Player extends React.Component {
-  render() {
-    return (<div
-        className={'b-player b-player--' + this.props.position + (this.props.active ? ' current' : '') }
-        key={this.props.id}>
-      <div className="b-player__name">
-        <span>
-          { this.props.current ? 'You (' + this.props.name + ')' : this.props.name }
-          <br/>
-        </span>
-      </div>
-      <div className="b-player__score">
-        {this.props.score}
-      </div>
-      <div className="b-player__avatar">
-        <img
-            src="https://cdn1.iconfinder.com/data/icons/ninja-things-1/720/ninja-background-128.png"
-            className="b-player__avatar-image"/>
-        <div className="b-player__marker c-mark" value={ this.props.marker }>
-          <Icon className="c-mark__x" name='remove' size="big" color="pink"/>
-          <Icon className="c-mark__o" name='circle thin' size="big" color="blue"/>
-        </div>
-      </div>
-    </div>);
-  }
-}
 
-export default class Players extends React.Component {
-  static defaultProps = {
-    count: 2, // - CountPlayers = 2
-    players: [],
-    currentPlayerId: null,
-    currentPlayerMove: null,
-    isHost: null
-  };
-
-  constructor() {
-    super();
-  }
-
+class Players extends Component {
   currentPositionLoader() {
     let position = 1;
     if ((this.props.isHost && this.props.currentPlayerMove !== this.props.currentPlayerId) ||
-        (!this.props.isHost && this.props.currentPlayerMove === this.props.currentPlayerId)) {
+      (!this.props.isHost && this.props.currentPlayerMove === this.props.currentPlayerId)) {
       position = 2;
     }
     return position;
   }
 
   render() {
-    let players = this.props.players.map((player) => {
+    const { players, scores } = this.props;
+    let playersList = players.map((player) => {
       return <Player
-          key={ player.id }
-          id={ player.id }
-          name={ player.name }
-          active={ player.id === this.props.currentPlayerMove }
-          current={ player.current }
-          marker={ player.marker }
-          position={ player.isHost ? 'host' : 'client' }
-          score={ this.props.scores[player.id] ? this.props.scores[player.id] : 0}
+        key={ player.id }
+        id={ player.id }
+        name={ player.name }
+        active={ player.id === this.props.currentPlayerMove }
+        current={ player.current }
+        marker={ player.marker }
+        position={ player.isHost ? 'host' : 'client' }
+        score={ scores[player.id] ? scores[player.id] : 0}
       />;
     });
     return (<div className="b-players">
       <div className="b-players__list">
-        {players}
+        {playersList}
       </div>
       <div className="b-players__current" value={ this.currentPositionLoader() }>
         <Popup
-            trigger={<div className="c-current">
-              <Icon loading name='spinner' size="big" color="white"/>
-            </div>}
-            content={ this.props.currentPlayerMove === this.props.currentPlayerId ? 'Ваш ход' : 'Игрок думает' }
-            position='top center'
+          trigger={<div className="c-current">
+            <Icon loading name='spinner' size="big" color="white"/>
+          </div>}
+          content={ this.props.currentPlayerMove === this.props.currentPlayerId ? 'Ваш ход' : 'Игрок думает' }
+          position='top center'
         />
       </div>
     </div>);
   }
 }
 
-export { Players };
+Player.propTypes = {
+  players: PropTypes.array,
+  currentPlayerId: PropTypes.string,
+  currentPlayerMove: PropTypes.bool,
+  isHost: PropTypes.bool
+};
+Player.defaultProps = {
+  players: [],
+  currentPlayerId: null,
+  currentPlayerMove: null,
+  isHost: null
+};
+
+export default Players;
