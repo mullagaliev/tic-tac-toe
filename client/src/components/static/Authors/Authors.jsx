33bd06f9f3fd128fc6db44fabd6453fcdf64dc25
Andrew <andrew@mullagaliev.com>
2018-07-16 16:33:51 +0300

    Испрпавил верстку авторов
    - Добавил ссылку на личный сайт, удалил ссылку на VK

--- client/src/components/static/Authors/Authors.jsx
+++ client/src/components/static/Authors/Authors.jsx
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
-import { Icon, Grid } from 'semantic-ui-react';
-import './Authors.sass';
+import { Icon } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const AuthorContainer = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  .b-authors-item{
+    width: 50px;
+    background-color: rgba(255, 255, 255, 0.74);
+    transition: all 0.33s;
+    padding: 10px 0;
+    &:hover{
+      border-radius: 10px;
+    }
+    .icon{
+      margin: 0 !important
+    }
+  }
+`;
 
 class Authors extends Component {
   render() {
     return (
-        <Grid columns={3}>
-          <Grid.Row className="b-author">
-            <Grid.Column>
-              <a href="https://dribbble.com/shots/2841696-X-O-Game-Design" target="_blank">
-                <div className="b-author__item">
-                  <Icon name='dribble' size='big'/>
-                </div>
-              </a>
-            </Grid.Column>
-            <Grid.Column>
-              <a href="https://github.com/mullagaliev" target="_blank">
-                <div className="b-author__item">
-                  <Icon name='github' size='big'/>
-                </div>
-              </a>
-            </Grid.Column>
-            <Grid.Column>
-              <a href="https://vk.com/vray1995" target="_blank">
-                <div className="b-author__item">
-                  <Icon name='vk' size='big'/>
-                </div>
-              </a>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <AuthorContainer>
+          <a className='b-authors-item' href="https://dribbble.com/shots/2841696-X-O-Game-Design"
+             target="_blank">
+            <Icon name='dribble' size='big'/>
+          </a>
+          <a className='b-authors-item' href="https://github.com/mullagaliev" target="_blank">
+            <Icon name='github' size='big'/>
+          </a>
+          <a className='b-authors-item' href="https://mullagaliev.com/" target="_blank">
+            <Icon name='rocket' size='big'/>
+          </a>
+        </AuthorContainer>
     );
   }
 }
